ace78617908b5ddfdba0cd80f2aa8f5550d9f0e7
SarahN28 <sarah.ngo@etu.cyu.fr>
2026-05-15 12:13:00 +0200

    Update positionJoueur.c

--- positionJoueur.c
+++ positionJoueur.c
@@ -1,13 +1,13 @@
-void placement(int k, Pivot place){  //numéro du joueur, penser à changer les cases qui bougent pas de 1 à 4!!!
+void placement(int k, Pivot place){                    //numéro du joueur, penser à changer les cases qui bougent pas de 1 à 4!!!
   int col = 87;
   int vide = 56;
   while (vide!=0){
-    printf ("Choisir un numéro de colonne.");  //demander à joueur colonne pour placer jeton
+    printf ("Choisir un numéro de colonne.");                 //demander à joueur colonne pour placer jeton
     scanf ("%d", &col);
     col--;
     place.colonne = col;
-    if (tab[0][col])!=0){  //vérifier que tab [0][j] vide
-     printf ("Cette case n'est pas disponible. Choisir une autre case.");  //si non afficher erreur et recommencer
+    if (tab[0][col])!=0){                      //vérifier que tab [0][j] vide
+     printf ("Cette case n'est pas disponible. Choisir une autre case.");             //si non afficher erreur et recommencer
      vide = 1;
     }
     else{
