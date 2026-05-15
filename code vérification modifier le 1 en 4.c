96351c325cbf66ebbc4d2bfa2cdcef34fc7cd194
Jenshika.J <jenshikaj05@gmail.com>
2026-05-15 13:22:49 +0200

    Update code vérification modifier le 1 en 4.c

--- code vérification modifier le 1 en 4.c
+++ code vérification modifier le 1 en 4.c
@@ -13,57 +13,35 @@
          if(j+4 < 8){
 //on compare les 5 cases
            if(tab[i][j+1] == valeur && tab[i][j+2] == valeur && tab[i][j+3] == valeur && tab[i][j+4] == valeur){
-             return 1;
+             return valeur;    //le joueur gagne
            }
          }
-       }
-     }
-   }
-
+      
 //verticale 
-   for(i = 0; i < 6; i++){
-     for(j = 0; j < 8 ; j++){
-     valeur = tab[i][j];
 //on verifie qu'il reste 4 cases en bas
-       if(valeur>1){
-         if(j+4 < 6){
-           if(tab[i+1][j] == valeur && tab[i+2][j] == valeur && tab[i+3][j] == valeur && tab[i+4][j] == valeur){
-             return 1;
+          if(j+4 < 6){
+            if(tab[i+1][j] == valeur && tab[i+2][j] == valeur && tab[i+3][j] == valeur && tab[i+4][j] == valeur){
+             return valeur;   //le joueur gagne
+            }
+         }
+  
+//diagonale droite
+//on verifie qu'il reste 4 cases en bas à droite
+         if(i+4 < 6 && j+4 < 8){
+           if(tab[i+1][j+1] == valeur && tab[i+2][j+2] == valeur && tab[i+3][j+3] == valeur && tab[i+4][j+4] == valeur){
+            return valeur;   //le joueur gagne
            }
          }
-       }
-     }
-   }
-
-//diagonale droite
-  for(i = 0; i < 6; i++){
-    for(j = 0; j < 8 ; j++){
-    valeur = tab[i][j];
-//on verifie qu'il reste 4 cases en bas à droite
-      if(valeur>1){
-        if(i+4 < 6 && j+4 < 8){
-          if(tab[i+1][j+1] == valeur && tab[i+2][j+2] == valeur && tab[i+3][j+3] == valeur && tab[i+4][j+4] == valeur){
-            return 1;
-          }
-        }
-      }
-    }
-  }
-
 
 //diagonale gauche
-  for(i = 0; i < 6; i++){
-    for(j = 0; j < 8 ; j++){
-    valeur = tab[i][j];
 //on verifie qu'il reste 4 cases en bas à gauche
-       if(valeur>1){
          if(i+4 < 6 && j-4 >= 0){
            if(tab[i+1][j-1] == valeur && tab[i+2][j-2] == valeur && tab[i+3][j-3] == valeur && tab[i+4][j-4] == valeur){
-             return 1;
+             return valeur;
            }
          }
        }
-    }
-  }
-return 0  //aucun victoire
+     }
+   }
+   return 0  //aucun victoire
 }
