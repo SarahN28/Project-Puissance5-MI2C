760c75fbc6bb0a0784588e7e2334e8d098a45f49
SarahN28 <sarah.ngo@etu.cyu.fr>
2026-05-19 18:49:20 +0200

    Update Main.c

--- Main.c
+++ Main.c
@@ -5,23 +5,24 @@
 
 
 int main () {
- int nvlig, nbpivot, nbcase;
+ int nwline, nbpivot, nbcase;
  int numerocol = 0;
- int finpartie = 1;
+ int endMatch = 1;
  int retF = 0;
  char carlu;
  char reprise = ' ';
+ char topinit = '0';
  pivot piv;
- jeu nvJeu;
+ Game nwGame;
  FILE *f = NULL;
  srand(time(NULL));
    
- f=fopen("SauvP5.txt", "rb") ;
+ f = fopen("SauvP5.txt", "rb") ;
  if(f!= NULL) {                                                                                                      // verification si sauvegarde du jeu existe
   while(reprise != 'o' && reprise != 'O' && reprise != 'n' && reprise != 'N') {
     do {
       if (reprise == '\n'){
-        printf("erreur 2 de saisie\n");
+        printf("erreur de saisie 2\n");
       }
       printf("Voulez vous reprendre l'ancienne partie ? O pour Oui, N pour Non \n");
       scanf("%c", &reprise);
@@ -29,48 +30,47 @@ int main () {
     carlu = getchar();
     while (carlu != '\n') {
       carlu = getchar();
-    }
-  }
-}
-if (reprise=='o' || reprise == 'O') {                                                                    // initialisation jeu avec sauvegarde
-  retF = fread(&nvJeu, sizeof(jeu), 1, f);
-  if (retF!=1) {
-    printf("Dernier jeux introuvable, vous allez commencer une nouvelle partie \n");
-  }
-}
-// initialisation du jeu si non récupérer du fichier de sauvegarde
-if (retF != 1) {
-  nvJeu.nbJoueur=constructJoueur(nvJeu.jo);
-  if (nvJeu.nbJoueur == 99) {
+     }
+   }
+  if (reprise=='o' || reprise == 'O') {                                                                    // initialisation jeu avec sauvegarde
+    retF = fread(&nwGame, sizeof(Game), 1, f);
+    if (retF!=1) {
+      printf("Dernier jeu introuvable, vous allez commencer une nouvelle partie \n");
+     }
+    else{
+      topinit = '1';
+     }
+   }
+  retF = fclose(f);
+  retF = remove("SauvP5.txt");
+ }
+if (topinit != '1') {
+  nwGame.nbPlayer=constructPlayer(nwGame.pl);
+  if (nwGame.nbPlayer == 99) {
     return 0;
-  }                                                               //finpartie
-  createPiste(nvJeu.grille, NBLIG, NBCOL);
-  nvJeu.numeroJoueur = 0; 
+  }                                                               //endMatch
+  createGrid(nwGame.grid, NBLIN, NBCOL);
+  nwGame.noPlayer = 0; 
 }
-if(f!= NULL) {                                                                                         // suppression du fichier de sauvegarde
- retF = fclose(f);
- retF = remove("SauvP5.txt");
-}
-printGame(nvJeu, NBLIG, NBCOL);
-while(finpartie == 1) {                                                                                  // a faire tant que partie non terminée
-  printf("Au tour du joueur %s \n", nvJeu.jo[nvJeu.numeroJoueur].nomJoueur);
-  nvlig = -1;
+
+printGame(nwGame, NBLIN, NBCOL);
+while(endMatch == 1) {                                                                                  // a faire tant que partie non terminée
+  printf("Au tour du joueur %s \n", nwGame.pl[nwGame.noPlayer].namePlayer);
+  nwline = -1;
    // calcul largeur carré à pivoter
-  nbpivot = selectionpivot();
+  nbpivot = sizePiv();
   nbcase = nbpivot/2;
-  
-  //insertion du jeton du nv joueur
-  while(nvlig==-1) {
+  while(nwline==-1) {                                                                                       //insertion du jeton du nv joueur
     // demander la colonne où insérer le jeton ou 99 pour sortir
-    numerocol = saisiecolonne(nvJeu.numeroJoueur); 
-    if (numerocol ==99) {
-      finpartie = 2;
+    numerocol = selectCol(nwGame.noPlayer); 
+    if (numerocol == 99) {
+      endMatch = 2;
       reprise = ' ';
       do {
         printf("Voulez vous sauvegarder la partie ? O pour Oui, N pour Non \n");
         scanf("%c", &reprise);
         if(reprise == 'o' || reprise == 'O') { 
-          sauvJeux(nvJeu, f);
+          sauvJeux(nwGame, f);
           return 0;
         }
         else if (reprise == 'n' || reprise == 'N'){
@@ -82,38 +82,38 @@ while(finpartie == 1) {                                                         
         carlu = getchar();
        }
     }
-    nvlig= deplacement_bas(nvJeu.grille, nvJeu.numeroJoueur+2, numerocol);                  // insertion du jeton : valeur jeton = indice  du joueur + 2
-   }                                                                                        // fin insertion du jeton : le nouveau pion sera en nvlig, numerocol
-  printGame(nvJeu, NBLIG, NBCOL);
-  piv = choixPivot(nbcase, nvlig, numerocol);                                               // demande position du pivot
-  if (piv.ligne==99) {
-    finpartie = 2;
+    nwline= fall(nwGame.grid, nwGame.noPlayer+2, numerocol);                  // insertion du jeton : valeur jeton = indice  du joueur + 2
+   }                                                                                        // fin insertion du jeton : le nouveau pion sera en nwline, numerocol
+  printGame(nwGame, NBLIN, NBCOL);
+  piv = selectPiv(nbcase, nwline, numerocol);                                               // demande position du pivot
+  if (piv.line==99) {
+    endMatch = 2;
     return 0;
    }
-  if (demanderRotation() ==1 ) {                                                            // demande du type de rotation
-   rotationHoraire(nvJeu.grille, nbcase, piv);
+  if (askRotation()==1 ) {                                                            // demande du type de rotation
+   Clockwise(nwGame.grid, nbcase, piv);
    }
   else {
-    rotationAntiHoraire(nvJeu.grille, nbcase, piv);
+    counterClock(nwGame.grid, nbcase, piv);
    }
-  printGame(nvJeu, NBLIG, NBCOL);
-  gravitePivot(nvJeu.grille, piv, nbcase);                                                 // gravité suite à la rotation
-  printGame(nvJeu, NBLIG, NBCOL);
-  finpartie = finJeu(nvJeu.grille, nvJeu.jo);                                              // si finpartie = 2 => Terminée pas de gagnant, si = 0 : gagnant
-  if (nvJeu.numeroJoueur >= nvJeu.nbJoueur-1) {                                            // calcul du nouveau joueur qui doit jouer
-    nvJeu.numeroJoueur = 0;
+  printGame(nwGame, NBLIN, NBCOL);
+  gravityPivot(nwGame.grid, piv, nbcase);                                                 // gravity suite à la rotation
+  printGame(nwGame, NBLIN, NBCOL);
+  endMatch = endGame(nwGame.grid, nwGame.pl);                                              // si endMatch = 2 => Terminée pas de gagnant, si = 0 : gagnant
+  if (nwGame.noPlayer >= nwGame.nbPlayer-1) {                                            // calcul du nouveau joueur qui doit jouer
+    nwGame.noPlayer = 0;
    }
    else {
-     nvJeu.numeroJoueur =  nvJeu.numeroJoueur+1;
+     nwGame.noPlayer =  nwGame.noPlayer+1;
     }
 }                                                                                         //fin de partie
-if (finpartie == 2) {                                                                      // affichage résultat 
+if (endMatch == 2) {                                                                      // affichage résultat 
   printf("La partie est terminée, SANS gagnant \n");
  }
  else {
    for (int i=0; i<3; i++) {
-     if (nvJeu.jo[i].aGagner == 1) {
-       printf("BRAVO au joueur %s, vous avez gagné\n", nvJeu.jo[i].nomJoueur);
+     if (nwGame.pl[i].Win == 1) {
+       printf("BRAVO au joueur %s, vous avez gagné\n", nwGame.pl[i].namePlayer);
       }
     }
   }
