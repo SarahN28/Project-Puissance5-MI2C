5d1c835d996f3245407ecc637c0a9bcff650e01f
SarahN28 <sarah.ngo@etu.cyu.fr>
2026-06-06 13:31:32 +0200

    Update essai.c

--- essai.c
+++ essai.c
@@ -1,1 +1,21 @@
+#define NBLIN 6
+#define NBCOL 8
 
+typedef struct {
+  int line;
+  int col;
+} pivot;
+
+typedef struct {
+  int numPlayer;
+  char namePlayer[11];
+  int Win;
+} Player;
+
+// les données du jeu : sont mises dans une structure pour pourvoir sauvegarder dans un fichier binaire, et les récupérer facilement
+typedef struct {
+  int nbPlayer;
+  int noPlayer;                           //prochain joueur à jouer
+  Player pl[3];
+  int grid[NBLIN][NBCOL];
+} Game;
